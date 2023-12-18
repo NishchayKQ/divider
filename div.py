0191c2da1105b4d9cf62e04b59aefec3bce7cad2
nishchay~ <108711354+NishchayKQ@users.noreply.github.com>
2023-12-18 18:19:08 +0530

    adding compability to decimal inputs

--- div.py
+++ div.py
@@ -1,4 +1,5 @@
 from itertools import cycle
+from fractions import Fraction
 
 default_color = "\033[0m"
 input_color = "\033[38;2;52;101;164m"
@@ -27,7 +28,7 @@ def print_note(text: str) -> None:
 
 peeps = int(input_colored("How many people are there? : "))
 
-total = 0
+total = Fraction("0")
 listOfPeeps = []
 totalDict = {}
 
@@ -38,45 +39,61 @@ for ara in range(peeps):
     print_output("Now enter the expenses paid by " + person)
     if (ara == 0):
         print_note("Note: Just enter all amounts paid by each person.\n"
-                   "The program will keep adding them up until you pass a 'blank enter'")
-    muns = 0
+                   "The program will keep adding them up until you\n"
+                   "pass a 'blank enter'\n")
+    muns = Fraction("0")
     while True:
         try:
             temp = input_colored(": ")
             if not temp:
                 break
-            temp = int(temp)
+            temp = Fraction(temp)
             total = temp + total
             muns = muns + temp
 
         except ValueError:
-            print_output("invalid input for an integer")
+            print_note("invalid input for a number")
 
     totalDict.update({person: muns})
 
-perPersonMuns = total / peeps
-print_output(f"total is {total} and per head {perPersonMuns}")
+perPersonMuns = Fraction(total / peeps)
+print_output(f"total is {format(total, ',.2f')}\n"
+             f"per head its {format(perPersonMuns, ',.2f')}")
 toPay = []
 
+print_note("\nbelow are the total amount each of you should\n"
+           "pay / recieve. You can use this to verify that\n"
+           "all dues have been cleared!")
+
 for ara in listOfPeeps:
-    temp = perPersonMuns - totalDict[ara]
-    print_output(f"money to be paid by {ara} is {temp}")
+    temp = Fraction(perPersonMuns - totalDict[ara])
+    if temp > 0:
+        print_output(f"\t{ara} has to pay {format(temp, ',.2f')}")
+    elif temp < 0:
+        print_output(f"\t{ara} has to receive {format(abs(temp), ',.2f')}")
+    else:
+        print_output(f"\t{ara} will not have to pay / receive anything!")
     toPay.append([temp, ara])
 
 
 toPay.sort()
 i = 0  # iterator that needs to be set
 j = 0  # iterator indicating loop position
+print_note("below are the exact transactions your group has to perform\n"
+           "so that there are no dues left!")
 for ara in toPay:
     if ara[0] < 0:
-        temp = abs(ara[0])
+        temp = Fraction(abs(ara[0]))
         try:
             i = j + 1
             while temp > 0:
                 if (toPay[i][0] > 0) and (abs(toPay[i][0]) <= temp):
                     temp = temp - toPay[i][0]
-                    print_output(
-                        f"{ara[1]} receives INR {toPay[i][0]} by {toPay[i][1]}")
+
+                    print_output(f"\t{toPay[i][1]} pays INR "
+                                 f"{format(toPay[i][0], ',.2f')} to "
+                                 f"{ara[1]}")
+
                     toPay[i] = [0, 0]
                 i = i + 1
         except IndexError:
@@ -84,15 +101,21 @@ for ara in toPay:
             while temp > 0:
                 if (toPay[i][0] > 0) and (abs(toPay[i][0]) <= temp):
                     temp = temp - toPay[i][0]
-                    print_output(
-                        f"{ara[1]} receives INR {toPay[i][0]} by {toPay[i][1]}")
+
+                    print_output(f"\t{toPay[i][1]} pays INR "
+                                 f"{format(toPay[i][0], ',.2f')} to "
+                                 f"{ara[1]}")
+
                     toPay[i] = [0, 0]
 
                 elif toPay[i][0] > 0:
-                    print_output(
-                        f"{ara[1]} receives INR {temp} by {toPay[i][1]}")
+
+                    print_output(f"\t{toPay[i][1]} pays INR "
+                                 f"{format(temp, ',.2f')} to "
+                                 f"{ara[1]}")
+
                     toPay[i][0] = toPay[i][0] - temp
-                    temp = 0
+                    temp = Fraction(0)
 
                 i = i + 1
 
